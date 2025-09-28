25155d26d560a7ec94b4e8ca05943daca69b0f90
Hamza Maqsood <hamzamaqsood072000@gmail.com>
2025-09-29 00:03:18 +0500

    Login page fixed + env var

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,23 +1,21 @@
-import React, { useState } from 'react'
-import "./Login.css"
-import { TextField } from '@mui/material'
-import { InputLabel } from '@mui/material'
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import IconButton from '@mui/material/IconButton';
-import InputAdornment from '@mui/material/InputAdornment';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import { FormControl } from '@mui/material';
-import { useForm } from 'react-hook-form';
-import { NavLink, useNavigate } from 'react-router-dom';
-
-
+import React, { useState } from "react";
+import "./Login.css";
+import { TextField } from "@mui/material";
+import { InputLabel } from "@mui/material";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import IconButton from "@mui/material/IconButton";
+import InputAdornment from "@mui/material/InputAdornment";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import { FormControl } from "@mui/material";
+import { useForm } from "react-hook-form";
+import { NavLink, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function Login() {
   const [showPassword, setShowPassword] = React.useState(false);
-  const [error, setError] = useState("")
+  const [error, setError] = useState("");
   const navigate = useNavigate();
-
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
@@ -33,78 +31,69 @@ export default function Login() {
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
-  } = useForm()
+  } = useForm();
 
   const onSubmit = async (data) => {
-    setError("")
-    const response = await fetch('http://localhost:3000/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ ...data }),
-      credentials: "include"
-    })
-    const info = await response.json();
-    if (!info.message) {
-      setError("")
-    }
-    else {
-      setError(info.message)
-    }
-    if (response.status == 200) {
-      navigate("/")
+    setError("");
+    try {
+      await axios.post(`${import.meta.env.VITE_BACKEND_URL}/auth/login`, data);
+      console.log("logged in successfully!");
+      navigate("/");
+    } catch (err) {
+      setError(err?.response?.data?.message)
     }
   };
 
   return (
     <>
-      <div className='loginParentDiv'>
-
-        <div className='loginContainer'>
-
-          <div className='loginHeading'>
+      <div className="loginParentDiv">
+        <div className="loginContainer">
+          <div className="loginHeading">
             <h3>Log in into your account</h3>
           </div>
 
-
-          <form className='loginForm' onSubmit={handleSubmit(onSubmit)} >
-            {
-              error && (
-                <div className='loginError'>
-                  {error}
-                </div>
-              )
-            }
+          <form className="loginForm" onSubmit={handleSubmit(onSubmit)}>
+            {error && <div className="loginError">{error}</div>}
 
             <TextField
               {...register("email", {
-                required: { value: true, message: "Email is required" }, pattern: {
+                required: { value: true, message: "Email is required" },
+                pattern: {
                   value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-                  message: "Invalid email address"
-                }
+                  message: "Invalid email address",
+                },
               })}
-
               sx={{ marginBottom: "18px" }}
               label="Enter Email"
-              margin='normal'
-              size='meduim'
+              margin="normal"
+              size="meduim"
               fullWidth
-              type='email'
+              type="email"
               error={!!errors.email}
               helperText={errors.email?.message}
             />
 
             <FormControl fullWidth variant="outlined">
-              <InputLabel htmlFor="outlined-adornment-password">Enter Password</InputLabel>
+              <InputLabel htmlFor="outlined-adornment-password">
+                Enter Password
+              </InputLabel>
               <OutlinedInput
-                {...register("password", { required: { value: true, message: "Password is required" }, minLength: { value: 8, message: "Password must have atleast 8 characters" }, maxLength: { value: 64, message: "Password must not be above 64 characters" } })}
-
+                {...register("password", {
+                  required: { value: true, message: "Password is required" },
+                  minLength: {
+                    value: 8,
+                    message: "Password must have atleast 8 characters",
+                  },
+                  maxLength: {
+                    value: 64,
+                    message: "Password must not be above 64 characters",
+                  },
+                })}
                 sx={{ marginBottom: "17px" }}
                 id="outlined-adornment-password"
-                type={showPassword ? 'text' : 'password'}
-                size='meduim'
-                margin='dense'
+                type={showPassword ? "text" : "password"}
+                size="meduim"
+                margin="dense"
                 fullWidth
                 endAdornment={
                   <InputAdornment position="end">
@@ -121,17 +110,19 @@ export default function Login() {
                 label="Enter Password"
               />
             </FormControl>
-            {errors.password && <div className='passwordError'>{errors.password.message}</div>}
+            {errors.password && (
+              <div className="passwordError">{errors.password.message}</div>
+            )}
 
-            <input className='loginButton' disabled={isSubmitting} type="submit" value="Login" />
-
+            <input
+              className="loginButton"
+              disabled={isSubmitting}
+              type="submit"
+              value="Login"
+            />
           </form>
-
-
-          <p>Don't have an account?<NavLink className='signupLink' to="/signup">Sign up</NavLink></p>
-
         </div>
       </div>
     </>
-  )
+  );
 }
